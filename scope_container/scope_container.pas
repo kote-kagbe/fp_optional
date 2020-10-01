9dc74ec014b495dbc6a0b6647eb2da7471f79869
Vasiliy Efimov <kote.kagbe@gmail.com>
2020-10-01 16:42:49 +0300

    added scope_container.pas

---
+++ scope_container/scope_container.pas
@@ -0,0 +1,74 @@
+unit scope_container;
+
+{$mode objfpc}{$H+}  
+{$modeswitch advancedrecords}
+
+interface
+
+uses
+    Classes, SysUtils;
+
+type
+    { tScopeContainer }
+
+    generic tScopeContainer<T: tObject> = record
+    private
+        _obj: T;
+
+        class operator initialize( var instance: tScopeContainer ); inline;
+        class operator finalize( var instance: tScopeContainer ); inline;
+    public
+        function get: T; inline;
+        procedure reset; inline;
+
+        class operator := ( const value: T ): tScopeContainer; inline;
+        class operator := ( const instance: tScopeContainer ): boolean; inline;
+        class operator not ( const instance: tScopeContainer ): boolean; inline;
+    end ;
+
+implementation
+
+{ tScopeContainer }
+
+class operator tScopeContainer.initialize ( var instance: tScopeContainer ) ;
+begin
+    instance._obj := default( T );
+end ;
+
+class operator tScopeContainer.finalize ( var instance: tScopeContainer ) ;
+begin
+    instance.reset;
+end ;
+
+function tScopeContainer.get: T;
+begin
+    result := _obj;
+end ;
+
+procedure tScopeContainer.reset;
+begin
+    if assigned( _obj ) then
+    begin
+        _obj.Free;
+        _obj := default( T );
+    end ;
+end ;
+
+class operator tScopeContainer. := ( const value: T ) : tScopeContainer;
+begin
+    result.reset;
+    result._obj := value;
+end ;
+
+class operator tScopeContainer. := ( const instance: tScopeContainer ) : boolean;
+begin
+    result := Assigned( instance._obj );
+end ;
+
+class operator tScopeContainer.not ( const instance: tScopeContainer) : boolean;
+begin
+    result := not Assigned( instance._obj );
+end ;
+
+end .
+
