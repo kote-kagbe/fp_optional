7b297fb126f7f3236b5284326862a8dcde953a75
efimovvp <efimovvp@tensor.ru>
2020-10-13 17:56:37 +0300

    Update string_key_storage.pas

--- string_key_storage/string_key_storage.pas
+++ string_key_storage/string_key_storage.pas
@@ -25,7 +25,7 @@ type
         class operator initialize( var instance: tSKSNodeRec ); inline;
         class operator finalize( var instance: tSKSNodeRec ); inline;
     public
-        constructor Init( const key: char );
+        procedure Init( const key: char );
     end;
 
     { tItemLocker }
@@ -39,7 +39,8 @@ type
         class operator initialize( var instance: tItemLocker ); inline;
         class operator finalize( var instance: tItemLocker ); inline;
     public
-        class operator := ( const value: _pSKSNodeRec ): tItemLocker; inline;
+        //class operator := ( const value: _pSKSNodeRec ): tItemLocker; inline;
+        procedure assign( const value: _pSKSNodeRec );
     end;
 
     generic tOnReleaseNode<ValueType> = procedure( const value: ValueType ) of object;
@@ -56,7 +57,7 @@ type
 
         _onReleaseNode: _tOnReleaseNode;
 
-        procedure releaseNode( const node: _tSKSNodeRec ); virtual;
+        function releaseNode( var node: _pSKSNodeRec; const reset_value: boolean ): boolean; virtual;
         function Get( const key: string ): _pSKSNodeRec; overload;
         function Get( const node: _pSKSNodeRec; out value: ValueType ): boolean; overload;
     public
@@ -64,8 +65,8 @@ type
 
         procedure Add( const key: string; const value: ValueType );
         function Get( const key: string; out value: ValueType ): boolean; overload;
-        function Get( const key: string; out value: ValueType; var locker: specialize tItemLocker<ValueType> ): boolean; overload;
-        //procedure Remove( const key: string );
+        function Get( const key: string; out value: ValueType; constref locker: specialize tItemLocker<ValueType>  ): boolean; overload;
+        procedure Remove( const key: string );
 
         procedure list( const key: string );
 
@@ -83,14 +84,18 @@ implementation
 
 class operator tItemLocker.initialize ( var instance: specialize tItemLocker<ValueType> ) ;
 begin
+    writeln( 'tItemLocker initialize ', longint(@instance) );
     instance._value := default( _pSKSNodeRec );
 end ;
 
 class operator tItemLocker.finalize ( var instance: tItemLocker ) ;
 begin
+    writeln( 'tItemLocker finalize ', longint(@instance) );
     if instance._value <> nil then
         begin
+            writeln( 'tItemLocker ->Acquire ', longint(@instance) );
             instance._value^._locker.Acquire;
+            writeln( 'tItemLocker Acquire-> ', longint(@instance) );
             instance._value^._ref_count -= 1;
             if( instance._value^._ref_count = 0 )and( not instance._value^._key ) then
                 dispose( instance._value )
@@ -99,32 +104,48 @@ begin
         end ;
 end ;
 
-class operator tItemLocker. := ( const value: _pSKSNodeRec ) : tItemLocker;
+// class operator tItemLocker. := ( const value: _pSKSNodeRec ) : tItemLocker;
+// begin
+//     if value = nil then
+//         exit;
+//     value^._locker.Acquire;
+//     result._value := value;
+//     result._value^._ref_count += 1;
+//     writeln( 'tItemLocker := ', result._value^._ref_count, ' ', longint(@result) );
+//     result._value^._locker.Release;
+// end ;
+
+procedure tItemLocker.assign( const value: _pSKSNodeRec );
 begin
     if value = nil then
         exit;
     value^._locker.Acquire;
-    result._value := value;
-    result._value^._ref_count += 1;
-    result._value^._locker.Release;
-end ;
+    _value := value;
+    _value^._ref_count += 1;
+    writeln( 'tItemLocker := ', _value^._ref_count, ' ', longint(@self) );
+    _value^._locker.Release;
+end;
 
 class operator tSKSNodeRec.initialize( var instance: tSKSNodeRec );
 begin
+    //writeln( 'tSKSNodeRec.initialize' );
     instance._key_count := 0;
     instance._next := nil;
     instance._sibling := nil;
     instance._value := default( ValueType );
     instance._ref_count := 0;
     instance._locker := TCriticalSection.Create;
+    instance._value.reset;
+    instance._key.reset;
 end;
 
 class operator tSKSNodeRec.finalize ( var instance: tSKSNodeRec ) ;
 begin
+    //writeln( 'tSKSNodeRec.finalize' );
     instance._locker.Free;
 end ;
 
-constructor tSKSNodeRec.Init( const key: char );
+procedure tSKSNodeRec.Init( const key: char );
 begin
     _key := key;
     //_key_count += 1;
@@ -135,7 +156,7 @@ var
     id: char;
 begin
     for id in key do
-       // writeln(id);
+       // //writeln(id);
 end;
 
 constructor tStringKeyStorage.Create;
@@ -144,11 +165,36 @@ begin
     
 end;
 
-procedure tStringKeyStorage.releaseNode( const node: _tSKSNodeRec );
+function tStringKeyStorage.releaseNode( var node: _pSKSNodeRec; const reset_value: boolean ): boolean;
 begin
-  
-    if( Assigned( _onReleaseNode ) )and( boolean( node._value ) )then
-        _onReleaseNode( node._value.get );
+    if node = nil then
+        exit;
+    writeln( '->releaseNode ', node^._key.get, ' ', boolean(node^._value), ' ', node^._ref_count );
+    node^._locker.Acquire;
+    //writeln( node^._key.get('~'), ' ', node^._key_count, ' ', node^._ref_count );
+    //writeln( 'node locked' );
+    node^._key_count -= 1;
+    if node^._key_count = 0 then
+        node^._key.reset;
+    result := node^._key;
+    if( node^._ref_count = 0 )and( not node^._key )then
+        begin
+            //writeln( 'dispose node' );
+            if( _onReleaseNode <> nil )and( boolean( node^._value ) )then
+                begin
+                    writeln( 'onReleaseNode ' );
+                    _onReleaseNode( node^._value.get );
+                end;
+            dispose( node );
+        end
+    else
+        begin
+            //writeln( 'node _key_count ', node^._key_count );
+            node^._locker.Release;
+            if reset_value then
+                node^._value.reset;
+        end;
+    //writeln( 'releaseNode->' );
 end;
 
 procedure tStringKeyStorage.Add( const key: string; const value: ValueType );
@@ -157,6 +203,7 @@ var
     node, current_node: _pSKSNodeRec;
     n, l: word;
 begin
+    //writeln( 'adding ', key );
     current_node := _root;
     node := nil;
     n := 0; // key item pos
@@ -171,15 +218,20 @@ begin
                     if node = nil then
                         begin
                             // creating new
-                            _root := new( _pSKSNodeRec );
+                            //writeln('new');
+                            new( _root );
+                            //writeln( 'init' );
                             _root^.Init( id );
+                            //writeln( 'cn assi' );
                             current_node := _root;
                             // adding to the current key chain
+                            //writeln( 'kc inc' );
                             current_node^._key_count += 1;
                             // creating empty _next for the next key item
                             if n < l then
                                 begin
-                                    node := new( _pSKSNodeRec );
+                                    //writeln( 'new next' );
+                                    new( node );
                                     current_node^._next := node;
                                     current_node := current_node^._next;
                                 end;
@@ -189,7 +241,7 @@ begin
                     else
                         begin
                             // creating new
-                            current_node := new( _pSKSNodeRec );
+                            new( current_node );
                             current_node^.Init( id );
                             // adding to the current key chain
                             current_node^._key_count += 1;
@@ -208,7 +260,7 @@ begin
                     // creating _next
                     if n < l then
                         begin
-                            node := new( _pSKSNodeRec );
+                            new( node );
                             current_node^._next := node;
                             current_node := current_node^._next;
                         end;
@@ -238,7 +290,7 @@ begin
                     if node = nil then
                         begin
                             // creating new
-                            node := new( _pSKSNodeRec );
+                            new( node );
                             // filling with current key item
                             node^.Init( id );
                             // adding sibling to the current node
@@ -250,7 +302,7 @@ begin
                             // creating _next
                             if n < l then
                                 begin
-                                    node := new( _pSKSNodeRec );
+                                    new( node );
                                     current_node^._next := node;
                                     current_node := current_node^._next;
                                 end;
@@ -270,8 +322,8 @@ begin
                         end;
                 end;
         end;
-    if( assigned( _onReleaseNode ) )and( boolean( current_node^._value ) ) then
-        _onReleaseNode( current_node^._value.get );
+    {if( assigned( _onReleaseNode ) )and( boolean( current_node^._value ) ) then
+        _onReleaseNode( current_node^._value.get );}
     current_node^._value := value;
 end;
 
@@ -283,7 +335,7 @@ var
 begin
     result := nil;
     l := length( key );
-    writeln( 'key length ', l );
+    //writeln( 'key length ', l );
     n := 0;
     current_node := _root;
     for id in key do
@@ -291,49 +343,49 @@ begin
             n += 1;
             if current_node = nil then
                 begin
-                    writeln( id, n, ' node is empty' );
+                    //writeln( id, n, ' node is empty' );
                     break;
                 end
             else if current_node^._key = id then
                 begin
-                    writeln( id, n, ' node found' );
+                    //writeln( id, n, ' node found' );
                     if n < l then
                         begin
-                            writeln( id, n, ' next' );
+                            //writeln( id, n, ' next' );
                             current_node := current_node^._next;
                         end;
                     continue
                 end
             else
                 begin
-                    writeln( id, n, ' parsing siblings' );
+                    //writeln( id, n, ' parsing siblings' );
                     current_node := current_node^._sibling;
                     while ( current_node <> nil )and( current_node^._key <> id ) do
                         begin
-                            writeln( id, n, ' sibling' );
+                            //writeln( id, n, ' sibling' );
                             current_node := current_node^._sibling;
                         end;
                     if current_node = nil then
                         begin
-                            writeln( id, n, ' sibling NOT found' );
+                            //writeln( id, n, ' sibling NOT found' );
                             break;
                         end
                     else
-                        writeln( id, n, ' sibling found' );
+                        //writeln( id, n, ' sibling found' );
                     if n < l then
                         begin
-                            writeln( id, n, ' next' );
+                            //writeln( id, n, ' next' );
                             current_node := current_node^._next;
                         end;
                 end;
         end;
     if ( current_node <> nil )and( boolean( current_node^._value ) ) then
         begin
-            writeln( id, n, ' value found' );
+            //writeln( id, n, ' value found' );
             result := current_node;
         end
     else
-        writeln( id, n, ' value NOT found' );
+        //writeln( id, n, ' value NOT found' );
 end ;
 
 function tStringKeyStorage.Get ( const node: _pSKSNodeRec; out value: ValueType ) : boolean;
@@ -341,7 +393,7 @@ begin
     value := default( ValueType );
     result := node <> nil;
     if result then
-        value := node^._value;
+        value := node^._value.get;
 end ;
 
 function tStringKeyStorage.Get( const key: string; out value: ValueType ): boolean;
@@ -349,25 +401,87 @@ begin
     result := Get( Get( key ), value );
 end;
 
-function tStringKeyStorage.Get( const key: string; out value: ValueType; var locker: specialize tItemLocker<ValueType> ): boolean;
+function tStringKeyStorage.Get( const key: string; out value: ValueType; constref locker: specialize tItemLocker<ValueType> ): boolean;
 var
     node: _pSKSNodeRec;
 begin
     node := Get( key );
     result := Get( node, value );
     if result then
-        locker := node;
+        //locker^ := node;
+        locker.assign( node );
 end ;
 
-{procedure tStringKeyStorage.Remove( const key: _KeyType );
+procedure tStringKeyStorage.Remove( const key: string );
+var
+    current_node, prev_node, next_node, sibl_node: _pSKSNodeRec;
+    id: char;
+    n, l: word;
 begin
-    
-end;}
-
-// procedure tStringKeyStorage.releaseNode;
-// begin
-  
-// end;
+    current_node := _root;
+    prev_node := nil;
+    next_node := nil;
+    l := length( key );
+    n := 0;
+    for id in key do
+        begin
+            n += 1;
+            //writeln( 'key ', id );
+            if current_node = nil then
+                begin
+                    //writeln( 'no current node' );
+                    break
+                end
+            else if current_node^._key = id then
+                begin
+                    //writeln( 'found node' );
+                    next_node := current_node^._next;
+                    if releaseNode( current_node, n=l ) then
+                        begin
+                            writeln( 'node released' );
+                            prev_node := nil;
+                        end
+                    else
+                        begin
+                            //writeln( 'node NOT released' );
+                            prev_node := current_node;
+                        end;
+                    current_node := next_node;
+                end
+            else
+                begin
+                    //writeln( 'parsing siblings' );
+                    prev_node := current_node;
+                    current_node := current_node^._sibling;
+                    while ( current_node <> nil ) and ( current_node^._key <> id ) do
+                        begin
+                            prev_node := current_node;
+                            current_node := current_node^._sibling;
+                        end;
+                    if current_node <> nil then
+                        begin
+                            //writeln( 'sibling found' );
+                            sibl_node := current_node^._sibling;
+                            next_node := current_node^._next;
+                            if releaseNode( current_node, n=l ) then
+                                begin
+                                    writeln( 'sibling released' );
+                                    if prev_node <> nil then
+                                        prev_node^._sibling := sibl_node;
+                                    prev_node := nil;
+                                end
+                            else
+                                begin
+                                    //writeln( 'sibling NOT released' );
+                                    prev_node := current_node;
+                                end;
+                            current_node := next_node;
+                        end
+                    else
+                        //writeln( 'sibling NOT found' );
+                end;
+        end;
+end;
 
 end.
 
